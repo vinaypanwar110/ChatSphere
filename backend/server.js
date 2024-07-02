4f903497a2e1b71ecec025ae2e5c0d8926e8fa28
vinaypanwar123 <vinaypanwar123s@gmail.com>
2024-07-02 21:21:42 +0530

    Implementing Socket.io online functionality

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDb.js";
 import cookieParser from "cookie-parser";
+import {app, server} from './socket/socket.js';
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
@@ -21,7 +21,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users",userRoutes);
 
-app.listen(PORT, async () => {
+server.listen(PORT, async () => {
   await connectToMongoDB();
   console.log(`Server Running on port ${PORT}`);
 });
--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -4,6 +4,7 @@ import Messages from "./Messages";
 import MessageInput from "./MessageInput";
 import { TiMessages } from "react-icons/ti";
 import useConversation from "../../zustand/useConversation";
+import { useAuthContext } from "../../context/AuthContext";
 const MessageContainer = () => {
   const { selectedConversation, setSelectedConversation } = useConversation();
   // const noChatSelected = true;
@@ -34,10 +35,11 @@ const MessageContainer = () => {
 export default MessageContainer;
 
 const NoChatSelected = () => {
+  const {authUser} = useAuthContext();
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <div className="px-4 text-center sm:text-lg md:text-gray-200 font-semibold flex flex-bold items-center gap-2">
-        <p>Welcome John depp </p>
+      <div className="px-4 text-center sm:text-lg md:text-gray-200 font-semibold flex flex-col items-center gap-2">
+        <p>Welcome {authUser.fullName} </p>
         <br />
         <p>Select a chat to start messaging</p>
         <TiMessages className="text-3xl md:text-6xl text-center"></TiMessages>
